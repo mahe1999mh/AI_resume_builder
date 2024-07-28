38459ee8f7f7418b1bb4f16e4f1bc957a1314e57
mahe1999mh <mahe1999mh@gmail.com>
2024-07-28 12:50:37 +0530

    chsnges

--- service/GlobalApi.js
+++ service/GlobalApi.js
@@ -2,12 +2,12 @@ import axios from "axios";
 
 
 const API_KEY=import.meta.env.VITE_STRAPI_API_KEY;
-// const axiosClient=axios.create({
-//     baseURL:import.meta.env.VITE_API_BASE_URL+"/api/",
-// })
 const axiosClient=axios.create({
-    baseURL:"https://apis-eta-smoky.vercel.app/api/"
+    baseURL:import.meta.env.VITE_API_BASE_URL+"/api/",
 })
+// const axiosClient=axios.create({
+//     baseURL:"https://apis-eta-smoky.vercel.app/api/"
+// })
 
 const CreateNewResume=(data)=>axiosClient.post('/user-resumes',data);
 
@@ -15,7 +15,7 @@ const GetUserResumes=(userEmail)=>axiosClient.get(`/user-resumes/${userEmail}`,)
 
 const UpdateResumeDetail=(id,data)=>axiosClient.put(`/user-resumes/${id}`,data)
 
-const GetResumeById=(id)=>axiosClient.get('/user-resumes/'+id)
+const GetResumeById=(id)=>axiosClient.get(`/user-resumes/getById/${id}`,)
 
 const DeleteResumeById=(id)=>axiosClient.delete('/user-resumes/'+id)
 
