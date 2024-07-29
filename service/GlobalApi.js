5f13da3489e33c68ff71831dcc4a36c4d8eb2721
Mahendra Reddy <122714066+mahe1999mh@users.noreply.github.com>
2024-07-29 10:23:14 +0530

    Update GlobalApi.js

--- service/GlobalApi.js
+++ service/GlobalApi.js
@@ -3,8 +3,11 @@ import axios from "axios";
 
 const API_KEY=import.meta.env.VITE_STRAPI_API_KEY;
 const axiosClient=axios.create({
-    baseURL:import.meta.env.VITE_API_BASE_URL+"/api/",
+    baseURL:"https://apis-eta-smoky.vercel.app/api/"
 })
+// const axiosClient=axios.create({
+//     baseURL:import.meta.env.VITE_API_BASE_URL+"/api/",
+// })
 // const axiosClient=axios.create({
 //     baseURL:"https://apis-eta-smoky.vercel.app/api/"
 // })
