5eaec51408ec88bf6cc3faf7e37b2ac9a39c70df
maendra <mahendra.reddy@relyonsoft.com>
2024-09-06 23:50:14 +0530

    changes

--- src/dashboard/resume/components/ResumePreview.jsx
+++ src/dashboard/resume/components/ResumePreview.jsx
@@ -5,28 +5,49 @@ import ExperiencePreview from "./preview/ExperiencePreview";
 import SummeryPreview from "./preview/SummeryPreview";
 import EducationalPreview from "./preview/EducationalPreview";
 import SkillsPreview from "./preview/SkillsPreview";
+import html2pdf from "html2pdf.js";
 
 const ResumePreview = () => {
-  const { resumeInfo, setResumeInfo } = useContext(ResumeInfoContext);
-  if (!resumeInfo) return;
+  const { resumeInfo } = useContext(ResumeInfoContext);
+  if (!resumeInfo) return null;
+
+  const generatePdf = () => {
+    const element = document.getElementById("content");
+
+    // Configuration for A4 size
+    const options = {
+      margin: [0, 0, 0, 0],
+      filename: "resume.pdf",
+      image: { type: "jpeg", quality: 1 },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
+    };
+
+    html2pdf().from(element).set(options).save();
+  };
+
   return (
-    <div
-      className="shadow-lg h-full p-14 border-t-[20px]"
-      style={{
-        borderColor: resumeInfo?.themeColor,
-      }}
-    >
-      {/* PersonalDetailPreview */}
-      <PersonalDetailPreview resumeInfo={resumeInfo} />
-      {/* Summery  */}
-      <SummeryPreview resumeInfo={resumeInfo} />
-      {/* Professional Experience  */}
-      <ExperiencePreview resumeInfo={resumeInfo} />
-      {/* Educational  */}
-      <EducationalPreview resumeInfo={resumeInfo} />
-      {/* Skilss  */}
-      <SkillsPreview resumeInfo={resumeInfo} />
-    </div>
+    <>
+      <div
+        className="shadow-lg h-full p-14 border-t-[20px]"
+        id="content"
+        style={{
+          borderColor: resumeInfo?.themeColor,
+        }}
+      >
+        {/* PersonalDetailPreview */}
+        <PersonalDetailPreview resumeInfo={resumeInfo} />
+        {/* Summery  */}
+        <SummeryPreview resumeInfo={resumeInfo} />
+        {/* Professional Experience  */}
+        <ExperiencePreview resumeInfo={resumeInfo} />
+        {/* Educational  */}
+        <EducationalPreview resumeInfo={resumeInfo} />
+        {/* Skills  */}
+        <SkillsPreview resumeInfo={resumeInfo} />
+      </div>
+      <button onClick={generatePdf}>Generate A4 PDF</button>
+    </>
   );
 };
 
