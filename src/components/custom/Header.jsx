e5844b9318bec3336f308a32c56cbc9284862267
maendra <mahendra.reddy@relyonsoft.com>
2024-10-17 17:36:52 +0530

    changes

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
 
   return (
     <div className="p-3 px-5 flex justify-between shadow-md">
-      <img src={"./logo.svg"} width={100} height={100} />
+      <img src={"./logo.png"} width={100} height={100} />
       {isSignedIn ? (
         <div className="flex gap-2 items-center">
           <Link to={"/dashboard"}>
